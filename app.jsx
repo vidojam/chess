069d277534ca4da0251652031c9e492d80a72743
Jose Torres <vidojam@gmail.com>
2024-08-06 12:26:09 -0400

    working on gameboard setup

--- app.jsx
+++ app.jsx
@@ -4,5 +4,24 @@ const infoDisplay = document.getElementById('#info-display');
 const width = 8;
 
 const startPieces = [
-
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook
+    
 ]
+
+function createBoard() {
+    startPieces.forEach((startpiece) => {
+        const piece = document.createElement('div');
+        square.classlist.add('square');
+        square.classlist.add('beige');
+        gameBoard.append(square);
+    })
+}
+
+createBoard();
