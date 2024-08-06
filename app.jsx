ddafa72b4f1eed5d7d57fa2308d7439e509a15ca
Jose Torres <vidojam@gmail.com>
2024-08-06 09:08:22 -0400

    start of coding for chess

---
+++ app.jsx
@@ -0,0 +1,8 @@
+const gameBoard = document.getElementById('#game-board');
+const playerDisplay = document.getElementById('#player');
+const infoDisplay = document.getElementById('#info-display');
+const width = 8;
+
+const startPieces = [
+
+]
