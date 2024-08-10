69353547f469949be737c365d8233bd7b14e9669
Jose Torres <vidojam@gmail.com>
2024-08-10 18:40:37 -0400

    changePlayer

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const gameBoard = document.getElementById('#game-board');
 const playerDisplay = document.getElementById('#player');
 const infoDisplay = document.getElementById('#info-display');
 const width = 8;
+let playerGo = 'black';
+playerDisplay.textContent = 'black'
 
 const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook,
@@ -20,6 +22,7 @@ function createBoard() {
         const piece = document.createElement('div');
         square.classlist.add('square');
         square.innerHTML = startpiece;
+        square.firstChild? setAttribute('draggable', 'true') : '';
         square.setAttribute(square-id, i);
         // square.classlist.add('beige');
         const row= Math.floor((63 - i) / 8) +1 ;
@@ -41,3 +44,67 @@ function createBoard() {
 }
 
 createBoard();
+
+const allSquares = document.querySelectorAll('.square');
+
+console.log(allSquares);
+
+allSquares.forEach(square => {
+    square.addEventListener('dragstart', dragStart)
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('drop', dragDrop)
+});
+
+let startPositionId
+let draggedElement
+
+function dragStart(e) {
+    startPositionId = e.target.parentNode.getAttribute('square-id');
+    draggedElement = e.target;
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragDrop(e) {
+    e.stopPropagation();
+    console.log(e.target);
+    const taken = e.target.classList.contains('piece');
+
+    // e.target.parentNode.append(draggedElement);
+    // e.target.remove();
+    // e.target.append(draggedElement);
+    
+    changePlayer();
+
+
+    // let endPositionId = e.target.parentNode.getAttribute('square-id');
+    // const endPosition = allSquares[endPositionId];
+    // endPosition.appendChild(draggedElement);
+    // endPosition.appendChild(draggedElement);
+}
+
+function changePlayer() {
+    if (playerGo === 'black') {
+        revertIds();
+        playerGo = 'white';
+        playerDisplay.textContent = 'white';
+    } else {
+        revertIds();
+        playerGo = 'black';
+        playerDisplay.textContent = 'black';
+    }
+}
+
+function reverseIds() {
+    const allSquares = document.querySelectorAll('.square')
+    allSquares.forEach((square, i) => 
+        square.setAttribute('square-id', (width * width - i)))
+}
+
+function revertIds () {
+    const allSquares = document.querySelectorAll('.square')
+    allSquares.forEach((square, i) => 
+        square.setAttribute('square-id', i))
+}
