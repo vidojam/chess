c026094ddd158c44bbf15c9b0ad1ce91f4394366
Jose Torres <vidojam@gmail.com>
2024-08-13 16:42:37 -0400

    checkForWin

--- app.js
+++ app.js
@@ -83,6 +83,7 @@ function dragDrop(e) {
         if (takenByOpponent && valid) {
             e.target.parentNode.append(draggedElement);
             e.target.remove();
+            checkForWin ()
             changePlayer();
             return;
         } 
@@ -97,6 +98,7 @@ function dragDrop(e) {
         // then check this
         if (valid) {
             e.target.append(draggedElement);
+            checkForWin ()
             changePlayer();
             return;
         }
@@ -330,4 +332,18 @@ function revertIds () {
     const allSquares = document.querySelectorAll('.square')
     allSquares.forEach((square, i) => 
         square.setAttribute('square-id', i))
+}
+
+function checkForWin () {
+    const kings = Array.from (document.querySelectorAll('#king'))
+    if (!kings.some(king => king.firstChild.classList.contains('white'))) {
+        infoDisplay.innerHTML('Black player wins!')
+        const allSquares = document.querySelectorAll('.square')
+        allSquares.forEach(square.firstChild?.setAttribute('draggable', 'false'))
+    } 
+    if (!kings.some(king => king.firstChild.classList.contains('black'))) {
+        infoDisplay.innerHTML('White player wins!')
+        const allSquares = document.querySelectorAll('.square')
+        allSquares.forEach(square.firstChild?.setAttribute('draggable', 'false'))
+    } 
 }
